68e478c030fb5eb239ee6f0a71882714380dde0a
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-14 15:14:42 -0400

    fix linters

--- src/item.rb
+++ src/item.rb
@@ -1,28 +1,33 @@
 require 'time'
 
 class Item
-	attr_reader :archived, :id
-	attr_accessor :genre, :author, :source, :label, :publish_date
+  @next_item_id = 0
 
-	def initialize(id, genre, author, source, label, publish_date, archived)
-		@id = id
-		@genre = genre
-		@author = author
-		@source = source
-		@label = label
-		@publish_date = publish_date
-		@archived = archived
-	end
+  def self.generate_id
+    @next_item_id += 1
+  end
 
-	def can_be_archived?
-		publish_date < (Time.now - (60*60*24*365*10))
-	end
+  attr_reader :archived, :id
+  attr_accessor :genre, :author, :source, :label, :publish_date
 
-	def move_to_archive
-		if can_be_archived?
-			@archived = true
-		end
-	end
+  def initialize(genre, author, source, label, publish_date)
+    @id = Item.generate_id
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = false
+  end
 
-	private :can_be_archived?
+  def can_be_archived?
+    publish_date < (Time.now - (60 * 60 * 24 * 365 * 10))
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+    @archived
+  end
+
+  private :can_be_archived?
 end
