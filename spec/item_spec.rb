41aa1c92d30ef87c5bdc133ffb4ae0e3ce083254
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-15 11:55:11 -0400

    Fix typos

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,14 +3,14 @@ require 'time'
 
 describe Item do
   before :each do
-    @archevable_item = Item.new(
+    @archivable_item = Item.new(
       'genre',
       'author',
       'source',
       'label',
       Time.now - (60 * 60 * 24 * 365 * 15)
     )
-    @non_archevable_item = Item.new(
+    @non_archivable_item = Item.new(
       'genre',
       'author',
       'source',
@@ -19,23 +19,23 @@ describe Item do
     )
   end
 
-  context 'Item Id is auto_incremente' do
-    it 'non_archevable_item and archevable_item has diferent ids' do
-      expect(@archevable_item.id != @non_archevable_item).to be(true)
+  context 'Item Id is auto_increment' do
+    it 'non_archivable_item and archivable_item has diferent ids' do
+      expect(@archivable_item.id != @non_archivable_item).to be(true)
     end
   end
 
-  context 'Item Id is auto_incremente' do
-    it 'archevable_item move_to_archive DOES archive the file' do
-      expect(@archevable_item.archived).to be(false)
-      @archevable_item.move_to_archive
-      expect(@archevable_item.archived).to be(true)
+  context 'Item Id is auto_increment' do
+    it 'archivable_item move_to_archive DOES archive the file' do
+      expect(@archivable_item.archived).to be(false)
+      @archivable_item.move_to_archive
+      expect(@archivable_item.archived).to be(true)
     end
 
-    it 'non_archevable_item move_to_archive DOES NOT archive the file' do
-      expect(@non_archevable_item.archived).to be(false)
-      @non_archevable_item.move_to_archive
-      expect(@non_archevable_item.archived).to be(false)
+    it 'non_archivable_item move_to_archive DOES NOT archive the file' do
+      expect(@non_archivable_item.archived).to be(false)
+      @non_archivable_item.move_to_archive
+      expect(@non_archivable_item.archived).to be(false)
     end
   end
 end
