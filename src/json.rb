bfa09e4707858b952bbe98d6917f1ef2bd97e481
andym80 <andymenutti@gmail.com>
2022-11-18 12:48:10 -0300

    Load Print & save at Json.rb

--- src/json.rb
+++ src/json.rb
@@ -24,4 +24,38 @@ module PreserveData
   def load_music_album
     get_data('music_album').map { |album| MusicAlbum.new(album['label'], album['author']) }
   end
+
+  def save_music_album(album)
+    data = get_data('music_album')
+    data << { label: album.label, author: album.author }
+    data_upgrade('music_album', data)
+
+    puts "The album '#{album.label.upcase}' by #{album.author.upcase} was created successfully!"
+  end
+
+  def load_books
+    get_data('books').map { |book| Book.new(book['title'], book['author']) }
+  end
+
+  def save_books(book)
+    data = get_data('books')
+    data << { title: book.title, author: book.author }
+    data_upgrade('books', data)
+
+    puts "The book '#{book.title.upcase}' by #{book.author.upcase} was created successfully!"
+  end
+end
+
+def load(_games)
+  get_data('games').map { |game| Game.new(game['title'], game['author']) }
+  end
+
+def save_games(game)
+  data = get_data('games')
+  data << { title: game.title, author: game.author }
+  data_upgrade('games', data)
+
+  puts "The game '#{game.title.upcase}' by #{game.author.upcase} was created successfully!"
+end
+
 end
